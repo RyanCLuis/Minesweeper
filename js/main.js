2e711df2806be016efa18e6301621507370631db
Ryan Luis <RyanCLuis@gmail.com>
2023-11-29 13:35:03 -0800

    changes html and Css to populate game board on start and adds some var

--- js/main.js
+++ js/main.js
@@ -1,28 +1,34 @@
-// loads page to a board (grid 8x8, Ants left:?, the x button) 
+// loads page to a board (grid 8x8, minds left:?, the x button) 
 
 // have the option to click a box in the grid or x toggle button
 
 // has three outcomes when clicking on a square:
-// 1. click on an ant
+// 1. click on a mind
     // - game will be over and have the button to play again
 
-// 2. click on square and get a number to tell you how many ants are near
+// 2. click on square and get a number to tell you how many mind are near
 
-// 3. click on a tile, when no ants are near by, will "flood the tiles" to the nearest hint.
+// 3. click on a tile, when no mind are near by, will "flood the tiles" to the nearest hint.
 
-// have the ability to place 'X' on the square to indicate there is an ant under it and wont be able to click on it
+// have the ability to place 'X' on the square to indicate there is a mind under it and wont be able to click on it
 
 /*----- constants -----*/
 
 
 /*----- state variables -----*/
+let board
+let winner
+let minds = 8
 
 
 /*----- cached elements  -----*/
+const messageEl = document.querySelector('h1')
+const headButton = document.querySelector('#head')
+const playAgainButton = document.querySelector('button')
+const tiles = [...document.querySelectorAll('#tiles > div')]
 
 
 /*----- event listeners -----*/
 
 
 /*----- functions -----*/
-console.log("JS is connected to my HTML!")
