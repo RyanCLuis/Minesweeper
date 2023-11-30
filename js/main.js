e1dcc49da633dcaa615c684c34e1a36bb82dd522
Ryan Luis <RyanCLuis@gmail.com>
2023-11-30 12:07:48 -0800

    makes board with tiles using js and adjusts some Css

--- js/main.js
+++ js/main.js
@@ -12,23 +12,41 @@
 
 // have the ability to place 'X' on the square to indicate there is a mind under it and wont be able to click on it
 
-/*----- constants -----*/
-
-
-/*----- state variables -----*/
-let board
-let winner
+let board = []
+let rows = 8
+let columns = 8
 let minds = 8
+// this will tell us the loction of the minds ex. 2-4, 0-1, etc
+let mindsLocation = []
+let tilesClicked = 0
+// this is to tell us when the flag button is toggled 
+let headEn = false
+let gameOver = false
+
+// const messageEl = document.querySelector('h1')
+// const headButton = document.querySelector('#head')
+// const playAgainButton = document.querySelector('button')
+// const tiles = [...document.querySelectorAll('#tiles > div')]
 
 
-/*----- cached elements  -----*/
-const messageEl = document.querySelector('h1')
-const headButton = document.querySelector('#head')
-const playAgainButton = document.querySelector('button')
-const tiles = [...document.querySelectorAll('#tiles > div')]
+window.onload = function() {
+    renderBoard()
+}
 
+function renderBoard() {
+    document.getElementById("mind-count").innerText = minds
+    // populate my tiles using a for loop
+    for (let r = 0;r < rows; r++) {
+        let row = []
+        for (let c = 0; c < columns; c++) {
+            // making a div tag in HTML
+            let tile = document.createElement("div")
+            tile.id = `${r}-${c}`
+            document.getElementById("tiles").append(tile)
+            row.push(tile)
+        }
+        board.push(row)
+    }
+    console.log(board)
+}
 
-/*----- event listeners -----*/
-
-
-/*----- functions -----*/
