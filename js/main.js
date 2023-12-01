ebbee032f1f202889671fe5306473ebf9c1ae754
Ryan Luis <RyanCLuis@gmail.com>
2023-12-01 12:38:17 -0800

    removes set minds and adds random mind genorator

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@
 let board = []
 let rows = 8
 let columns = 8
-let minds = 6
+let minds = 8
 // this will tell us the loction of the minds ex. 2-4, 0-1, etc
 let mindsLocation = []
 let tilesClicked = 0
@@ -27,7 +27,19 @@ let gameOver = false
 
 // we are setting specific locations for now, until the end to where we can randomize them!
 function setMinds() {
-    mindsLocation.push("2-2", "2-4", "0-0", "5-6", "3-4", "1-1")
+    // mindsLocation.push("2-2", "2-4", "0-0", "5-6", "3-4", "1-1")
+
+    let mindsLeft = minds;
+    while (mindsLeft > 0) { 
+        let r = Math.floor(Math.random() * rows);
+        let c = Math.floor(Math.random() * columns);
+        let id = r.toString() + "-" + c.toString();
+
+        if (!mindsLocation.includes(id)) {
+            mindsLocation.push(id);
+            mindsLeft -= 1;
+        }
+    }
 }
 
 function renderBoard() {
